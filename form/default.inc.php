b1839eeac0f4702d35ab0fb8ac9d83ef7eacdf81
Maddie <madelinecurnan@ufl.edu>
2023-02-01 13:42:52 -0500

    added files from laptop

---
+++ form/default.inc.php
@@ -0,0 +1,10 @@
+<header>
+  <nav>
+    <a href="index.html">Go Back To The Homepage</a>
+  </nav>
+</header>
+
+
+<div class="form-container">
+  <h1>Sorry something went wrong, please try again</h1>
+</div>
---
+++ js/main.js
@@ -0,0 +1,61 @@
+// Accordion Menu
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var innerAcc = document.getElementsByClassName("inner-accordion");
+var i;
+
+for (i = 0; i < innerAcc.length; i++) {
+  innerAcc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+// Slideshow from W3
+var slideIndex = [1,1,1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
